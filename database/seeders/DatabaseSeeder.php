eb52053c17b3271b92df96abccf760c64d05d3c3
Muhammad Al Farizi <mhmmd.alfrizi85@gmail.com>
2026-07-16 15:31:14 +0700

    Menambahkan seeder divisi, jabatan, dan user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DivisionSeeder::class,
             PositionSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
